1005a6713500b311ff4410c742c114bbae3a6d1a
Abdulmoiz Ahmer <abdulmoizahmer1996@gmail.com>
2022-11-01 19:36:46 +0500

    EDITS: section one almost completed

--- src/components/Steps/Categorization/index.js
+++ src/components/Steps/Categorization/index.js
@@ -9,6 +9,13 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import FormGroup from "@mui/material/FormGroup";
+import Checkbox from "@mui/material/Checkbox";
 
 export const Categorization = function (props) {
   return (
@@ -27,31 +34,45 @@ export const Categorization = function (props) {
             <Typography variant="body2" gutterBottom>
               Please select your type.
             </Typography>
-            <Card sx={{ minWidth: 275 }}>
+            <Card sx={{ minWidth: 275 }} className="radio-group-container">
               <CardContent>
-                <Typography
-                  sx={{ fontSize: 14 }}
-                  color="text.secondary"
-                  gutterBottom
-                >
-                  Word of the Day
-                </Typography>
-                <Typography variant="h5" component="div">
-                  belent
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  adjective
-                </Typography>
-                <Typography variant="body2">
-                  well meaning and kindly.
-                  <br />
-                  {'"a benevolent smile"'}
-                </Typography>
+                <FormControl>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="female"
+                    name="radio-buttons-group"
+                  >
+                    <FormControlLabel
+                      value="Digging Contractor"
+                      control={<Radio />}
+                      label="Digging Contractor"
+                    />
+                    <Typography variant="subtitle">
+                      Description will be here for the type.
+                    </Typography>
+                    <FormControlLabel
+                      value="Private Transporter"
+                      control={<Radio />}
+                      label="Private Transporter"
+                    />
+                    <Typography variant="subtitle">
+                      Description will be here for the type.
+                    </Typography>
+                    <FormControlLabel
+                      value="Construction and commercial waste transportation company"
+                      control={<Radio />}
+                      label="Construction and commercial waste transportation company"
+                    />
+                    <Typography variant="subtitle">
+                      Description will be here for the type.
+                    </Typography>
+                  </RadioGroup>
+                </FormControl>
               </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
             </Card>
+            <Button variant="contained" color="success">
+              Next
+            </Button>
           </Grid>
           <Grid item xs={6}>
             <Typography variant="subtitle2" gutterBottom>
@@ -62,28 +83,34 @@ export const Categorization = function (props) {
             </Typography>
             <Card sx={{ minWidth: 275 }}>
               <CardContent>
-                <Typography
-                  sx={{ fontSize: 14 }}
-                  color="text.secondary"
-                  gutterBottom
-                >
-                  Word of the Day
-                </Typography>
-                <Typography variant="h5" component="div">
-                  belent
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  adjective
-                </Typography>
-                <Typography variant="body2">
-                  well meaning and kindly.
-                  <br />
-                  {'"a benevolent smile"'}
-                </Typography>
+                <FormGroup>
+                  <FormControlLabel
+                    control={<Checkbox defaultChecked />}
+                    label="Commercial Activities"
+                    color="success"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox />}
+                    label="Construction Waste"
+                    color="success"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox />}
+                    label="Special Medical waste"
+                    color="success"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox />}
+                    label="Treated medical waste"
+                    color="success"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox />}
+                    label="Sewage Water"
+                    color="success"
+                  />
+                </FormGroup>
               </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
             </Card>
           </Grid>
         </Grid>
